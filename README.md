cfa8e2694e378e6db9e95f679f9e0db5b4f56c17
Luis Ricardo Celestino de Souza <souza.luis@gmail.com>
2018-12-31 18:08:14 -0300

    Code updated.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ASP_NET_Core_MVC
-Using ASP.NET MVC5.
+Using ASP.NET Core MVC.
---
+++ WebApplication1/Controllers/PessoaController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public static List<Pessoa> Lista { get; set; }
+
+        public PessoaController()
+        {
+            if (Lista == null)
+            {
+                Lista = new List<Pessoa>();
+            }
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Mensagem = "Nossa primeira view";
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Pessoa model)
+        {
+            ModelState.Remove("Codigo");
+
+            if (ModelState.IsValid)
+            {
+                model.Codigo = Lista.Count + 1;
+                Lista.Add(model);
+            }
+            else
+            {
+                return View(model);
+            }
+
+            return View("List", Lista);
+        }
+
+        public IActionResult List()
+        {
+            if (Lista != null)
+            {
+                return View(Lista);
+            }
+            return View(new List<Pessoa>());
+        }
+
+        public IActionResult Edit(int id)
+        {
+            if (Lista.Where(p => p.Codigo == id).Any())
+            {
+                var model = Lista.Where(p => p.Codigo == id).FirstOrDefault();
+
+                return View(model);
+            }
+
+            return View(new Pessoa());
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Pessoa model)
+        {
+            if (Lista != null)
+            {
+                if (Lista.Where(p => p.Codigo == model.Codigo).Any())
+                {
+                    //var modelBase = Lista.Where(p => p.Codigo == model.Codigo).FirstOrDefault();
+
+                    // Atualiza seu registro com o model enviado por parametro...
+                    Lista[model.Codigo - 1] = model;
+                }
+            }
+
+            return View("List", Lista);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (Lista != null)
+            {
+                if (Lista.Where(p => p.Codigo == id).Any())
+                {
+                    var modelBase = Lista.Where(p => p.Codigo == id).FirstOrDefault();
+                    Lista.Remove(modelBase);
+                    return View("List", Lista);
+                }
+            }
+
+            return View("List", new List<Pessoa>());
+        }
+        
+    }
+}
